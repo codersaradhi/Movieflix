db765e98a19c80ae209ee2bb163299d9b0b124c0
codersaradhi <chaitanyasaradhi1999@gmail.com>
2025-02-21 15:30:21 +0530

    ignore updated

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,6 +1,7 @@
 export const checkValidData =  (email,Password,name) =>{
     const validEmail = /^[a-zA-Z0-9_.±]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/.test(email) //email will be tested and returns a boolen value true or false
     const validPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(Password)
+    
    
 
     
