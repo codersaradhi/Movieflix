b7d3e10426e28317f86b7a9187cc01a6c3fbd48d
codersaradhi <chaitanyasaradhi1999@gmail.com>
2025-05-10 12:55:39 +0530

    updated the env variables

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -10,7 +10,7 @@ import { getAuth } from "firebase/auth"
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
   appId: process.env.REACT_APP_FIREBASE_APPID,
